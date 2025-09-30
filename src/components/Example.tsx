1c20dc3fe381b2d7d392fa2241b935aaaf54cb35
Nylbert Victor <nyldrift@hotmail.com>
2025-09-29 22:14:34 -0300

    feat: Configura estrutura inicial do projeto com React e TS

---
+++ src/components/Example.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Example: React.FC = () => {
+    return (
+        <div>
+            <h1>This is an example component!</h1>
+        </div>
+    );
+};
+
+export default Example;
